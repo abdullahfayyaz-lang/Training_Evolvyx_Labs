b66560068695bc22477618e810e8d24f0f0ad786
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-03 22:02:51 +0500

    Pyhton datatypes,variables

---
+++ start.py
@@ -0,0 +1,19 @@
+name="Abdullah Fayyaz"
+age=22
+speed=19.23
+Software_Engineer=True
+#data types
+##string, integer, float, boolean, None
+#Python is case sensitive language
+print("My name is",name,"and my age is",age)
+print("My speed is",speed,"km/h")
+print("I am a Software Engineer:",Software_Engineer)
+print(type(name))
+print(type(age))
+print(type(speed))
+print(type(Software_Engineer))
+
+a=10
+b=500
+sum=a+b
+print("The sum of a and b is:",sum)
