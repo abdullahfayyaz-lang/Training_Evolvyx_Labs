3f4c037cffecdee8d1a19dd436009bdd0031169b
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-07 17:45:39 +0500

    pythom type casting->ft mypy

---
+++ typing.py
@@ -0,0 +1,9 @@
+def surface_area_of_cube(edge_length: float) -> str:
+    return f"The surface area of the cube is {6 * edge_length ** 2}."
+
+#The function surface_area_of_cube takes an argument expected to be an instance of float, as indicated by the type hint edge_length: float. The function is expected to return an instance of str, as indicated by the -> str hint.
+
+
+number: int=100 # numbers can only bt int now 
+text: str="hello World"
+tpl:tuple=(1,3)
