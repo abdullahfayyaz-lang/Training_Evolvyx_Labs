2b8046cb505f4492fdd964936b025e283af0ff40
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-08 20:29:30 +0500

    Python django->middleware: process_view,process_exception,process_template_response

--- start/home/middleware.py
+++ start/home/middleware.py
@@ -1,5 +1,5 @@
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 #A middleware can be written as a function that looks like this:
 def new_middleware(get_response):
     def middleware(request):
@@ -31,9 +31,41 @@ class NewMiddleware2:
         print("View args:", view_args)
         print("View kwargs:", view_kwargs)
         #here i check if the student id is 100 it only then calls the view else just sends the reponse that this was blocked byt the middleware
-        if view_kwargs.get('id') != 100:
-            return HttpResponse("Access denied by middleware")
+        if view_func.__name__== "edit_student":
+                if view_kwargs.get('id') != 100:
+                    return HttpResponse("Access denied! Invalid ID.", status=403)
 
+
+    # process_exception() is a class-based middleware hook in Django that is triggered only if the view raises an exception.
+    # It allows you to:
+    # Handle or log exceptions globally
+    # Return a custom error response
+    # Prevent Django’s default error page from showing
+
+    def process_exception(self, request, exception):
+        print(">>> process_exception called")
+        print("Exception message:", exception)
+
+        # Return a custom response instead of Django's default error page
+        return JsonResponse({
+            "error": "Something went wrong in the view!",
+            "details": str(exception)
+        }, status=500)
+
+    # process_template_response() is called after the view is executed,
+    # but only if the view’s response is a “template response” —
+    # that means the view returns an object that has a .render() method, like:
+    # from django.template.response import TemplateResponse
+    # So this hook lets you:
+    # Modify the template context or template name
+    # Add or change data before rendering the template
+    def process_template_response(self, request, response):
+        print(">>> process_template_response called")
+        # Add extra context data before rendering
+        response.context_data["extra_message"] = "Added by middleware ✨"
+
+        # Optionally, you can even change the template:
+        response.template_name = "index.html"
 
 #middle ware cycle 
 
