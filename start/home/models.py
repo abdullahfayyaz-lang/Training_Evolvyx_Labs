44ffda5c4f7af467f2862a0aea473073534f0b86
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-09 00:16:22 +0500

    Python django->ORM Model Relationship(1-1,1-Many,Many-Many)

--- start/home/models.py
+++ start/home/models.py
@@ -34,7 +34,58 @@ class Album(models.Model):
 
 class Song(models.Model):
     name = models.CharField(max_length = 100)
-    album = models.ForeignKey(Album, on_delete = models.CASCADE)
+    album = models.ForeignKey(Album, on_delete = models.CASCADE)##Adding foreign key
 
     def __str__(self):
         return self.name
+
+#Many-to-many relationships->
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ["headline"]
+
+    def __str__(self):
+        return self.headline
+
+#One-to-one relationships
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.name} the place"
+
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
+
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s the waiter at %s" % (self.name, self.restaurant)
+
