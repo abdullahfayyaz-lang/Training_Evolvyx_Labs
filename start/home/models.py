a6cd1ec0461025aac7dfdc4f07891be18c59b9f5
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-09 18:12:14 +0500

    Python django-> Model Manager and Custom Query sets

--- start/home/models.py
+++ start/home/models.py
@@ -1,5 +1,27 @@
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class PersonManager(models.Manager):
+    pass  # You can add custom query methods later
+
+
+class Person(models.Model):
+    ROLE_CHOICES = [
+        ("A", _("Author")),
+        ("E", _("Editor")),
+    ]
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+
+    people = PersonManager()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.get_role_display()})"
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=20)
@@ -9,12 +31,24 @@ class Customer(models.Model):
         return self.first_name + " " + self.last_name
 
 
+class StudentManagerBSCS(models.Manager):#Fixed section filter
+    def get_queryset(self):
+        return super().get_queryset().filter(degree="BSCS")
+
+class StudentSectionManager(models.Manager):#Dynamic filtering
+    def for_section(self, section_name):#here we didnt used the query_set bcz it does not accepts the 2nd arrgument so we have to create a custome one
+        return self.get_queryset().filter(section=section_name)
 
 class Student(models.Model):
     name=models.CharField(max_length=50)
     student_id=models.IntegerField()
     section=models.CharField(max_length=5)
     degree=models.CharField(max_length=4)
+
+    objects=models.Manager()#default manager
+    Bscs_objects=StudentManagerBSCS()## will only return students with degree bscs
+    Section_objects=StudentSectionManager()#return all students of a specific section
+
 
 
 class Author(models.Model):
@@ -89,3 +123,41 @@ class Waiter(models.Model):
     def __str__(self):
         return "%s the waiter at %s" % (self.name, self.restaurant)
 
+
+class PersonQuerySet(models.QuerySet):#Custom Querry sets
+    def authors(self):
+        return self.filter(role="A")
+
+    def editors(self):
+        return self.filter(role="E")
+
+
+    def starts_with(self,letter):
+        return self.filter(first_name__istartswith=letter)
+
+
+class PersonManager(models.Manager):
+    def get_queryset(self):
+        return PersonQuerySet(self.model, using=self._db)
+
+    def authors(self):
+        return self.get_queryset().authors()
+
+    def editors(self):
+        return self.get_queryset().editors()
+
+
+class Person(models.Model):
+    ROLE_CHOICES = [
+        ("A", _("Author")),
+        ("E", _("Editor")),
+    ]
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+
+    people = PersonManager()
+    objects=models.Manager()#default manager
+
+    
