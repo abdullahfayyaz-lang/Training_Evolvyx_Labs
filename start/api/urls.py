de8bd9dd286d8fe85e271c03db0214f15b372db2
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-10 22:17:22 +0500

    Python Django -> REST Framework Nested Serializers

--- start/api/urls.py
+++ start/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 from . import views
 
-urlpatterns=[path('products/',views.product_list)]
+urlpatterns=[path('products/',views.product_list),path('products/<int:pk>/',views.product_details),path('orders/',views.order_list),path('orders/<str:pk>/',views.order_details)
+]
--- start/api/views.py
+++ start/api/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer,OrderItemSerializer,OrderSerializer
+from api.models import Product,Order,OrderItem
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -11,4 +11,22 @@ from rest_framework.decorators import api_view
 def product_list(request):
     products=Product.objects.all()
     serializer=ProductSerializer(products,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_details(request,pk):
+    products=get_object_or_404(Product,pk=pk)
+    serializer=ProductSerializer(products)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_list(request):
+    orders=Order.objects.all()
+    serializer=OrderSerializer(orders,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_details(request,pk):
+    order=get_object_or_404(Order,pk=pk)
+    serializer=OrderSerializer(order)
     return Response(serializer.data)
