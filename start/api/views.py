b6c391593571f4c34298382a169745f6319c9f23
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-10 23:25:13 +0500

    Python Django -> REST Framework ViewSet Actions

--- start/api/views.py
+++ start/api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from api.serializers import ProductSerializer,OrderItemSerializer,OrderSerializer
 from api.models import Product,Order,OrderItem
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,action
 from rest_framework import viewsets
 # Create your views here.
 
@@ -36,3 +37,8 @@ def order_details(request,pk):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset=Order.objects.all()
     serializer_class=OrderSerializer
+    @action(detail=False,methods=['get'],url_path='user-orders')
+    def user_orders(self,request):
+        orders=self.get_queryset().filter(user=request.user)
+        serializer=self.get_serializer(orders,many=True)
+        return Response(serializer.data)
