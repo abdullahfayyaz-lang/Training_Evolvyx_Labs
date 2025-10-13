d6b87fa0beb12cb2b1978178bacf89da34012fc0
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-13 20:50:57 +0500

    Pyhton Django -> JWT Authentication|IsAuthenticated|AllowAny|IsAdmin

--- start/api/admin.py
+++ start/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import Order , OrderItem
+from api.models import Order , OrderItem, User
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
     model=OrderItem
@@ -9,4 +9,6 @@ class OrderAdmin(admin.ModelAdmin):
         OrderItemInline
     ]
 
+
 admin.site.register(Order,OrderAdmin)
+admin.site.register(User)
--- start/start/settings.py
+++ start/start/settings.py
@@ -136,3 +136,10 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'api.User'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
