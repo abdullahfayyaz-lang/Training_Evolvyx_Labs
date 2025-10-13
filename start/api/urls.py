0659effa82d59996203d4a6f8f0f4146b2d0b43d
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-13 17:35:21 +0500

    Python Django -> Generic Views|ListAPIView|RetrieveAPIView

--- start/api/urls.py
+++ start/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter 
 
-urlpatterns=[path('products/',views.product_list),path('products/<int:pk>/',views.product_details)
+urlpatterns=[path('products/',views.ProductListAPIView.as_view()),path('products/<int:pk>/',views.ProductDetailAPIView.as_view())
 ]
 
 router = DefaultRouter()
--- start/api/views.py
+++ start/api/views.py
@@ -2,24 +2,33 @@ from django.shortcuts import render,get_object_or_404
 from django.http import JsonResponse
 from api.serializers import ProductSerializer,OrderItemSerializer,OrderSerializer
 from api.models import Product,Order,OrderItem
-
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,action
 from rest_framework import viewsets
 # Create your views here.
 
-@api_view(['GET'])
-def product_list(request):
-    products=Product.objects.all()
-    serializer=ProductSerializer(products,many=True)
-    return Response(serializer.data)
+class ProductListAPIView(generics.ListAPIView):
+    queryset=Product.objects.filter(stock__gt=0)# now it will only return the products with stock greater then 0
+    serializer_class=ProductSerializer
 
-@api_view(['GET'])
-def product_details(request,pk):
-    products=get_object_or_404(Product,pk=pk)
-    serializer=ProductSerializer(products)
-    return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    lookip_url_kwarg="product_id"##to chnage url parameter else then pk we use this 
+# @api_view(['GET'])
+# def product_list(request):
+#     products=Product.objects.all()
+#     serializer=ProductSerializer(products,many=True)
+#     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def product_details(request,pk):
+#     products=get_object_or_404(Product,pk=pk)
+#     serializer=ProductSerializer(products)
+#     return Response(serializer.data)
 
 @api_view(['GET'])
 def order_list(request):
