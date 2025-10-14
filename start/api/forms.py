99e8ccc1cbea3de4669eb877a20b5a2724556b20
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-15 00:22:39 +0500

    Python Django -> Input Validation

--- start/api/forms.py
+++ start/api/forms.py
@@ -6,3 +6,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        def clean_email(self):
+            email=self.cleaned_data.get('email')
+            if '@example.com' not in email:
+                raise forms.ValidationError('Invalid Email Domain .')
+            return email
--- start/api/models.py
+++ start/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
-
+from django.core.exceptions import ValidationError
 
 class User(AbstractUser):
     pass
@@ -10,7 +10,7 @@ class User(AbstractUser):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)#input validation
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='products/', blank=True, null=True)
 
@@ -20,6 +20,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    def clean(self):
+        if len(self.name)<2:
+            raise ValidatoionError('Name must be at least 2 characters long')
 
 
 class Order(models.Model):
