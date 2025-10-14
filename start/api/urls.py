adc4f26c3ba56155f385c3e02822104c56ebea9a
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-14 21:35:12 +0500

    Python Django -> User Authentication(django)|user signup | user login

--- start/api/urls.py
+++ start/api/urls.py
@@ -6,7 +6,10 @@ from rest_framework.routers import DefaultRouter
 urlpatterns=[path('products/',views.ProductListCreateAPIView.as_view()),
 path('products/<int:pk>/',views.ProductDetailAPIView.as_view(),name='product-detail'),
 path('user-orders/',views.UserOrderAPIListView.as_view(),name='user_orders'),
-path('users/',views.UserListView.as_view(),name='users')
+path('users/',views.UserListView.as_view(),name='users'),
+path('signup/', views.signup, name='signup'),
+path('login/', views.login_view, name='login'),
+path('logout/', views.logout_view, name='logout'),
 ]
 
 router = DefaultRouter()
--- start/api/views.py
+++ start/api/views.py
@@ -14,6 +14,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework.throttling import ScopedRateThrottle
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm
+
+from django.contrib import messages
+from .models import User
+
 # Create your views here.
 
 # class ProductListAPIView(generics.ListAPIView):
@@ -132,3 +139,36 @@ class UserListView(generics.ListAPIView):
     permission_classes=[IsAuthenticated]
     serializer_class=UserSeralizer
     pagination_class=None
+
+# Signup View
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('login')
+        else:
+            messages.error(request, "Please correct the error below.")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+# Login View
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to the homepage or dashboard
+        else:
+            messages.error(request, "Invalid credentials.")
+    return render(request, 'login.html')
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    return redirect('login')
