c8a64788bc5805cf39e79b1ebe38870f4394d00c
abdullahfayyaz-lang <abdullah.fayyaz@evolvyxlabs.com>
2025-10-14 18:01:24 +0500

    Python Django DRF -> Creating Nested Objects | overriding serailizer create()

--- start/api/serializers.py
+++ start/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Product,Order,OrderItem
+from django.db import transaction
+
 
 
 
@@ -34,12 +36,30 @@ class OrderCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         orderitem_data = validated_data.pop('items')
-        order = Order.objects.create(**validated_data)
+        with transaction.atomic():
+            order = Order.objects.create(**validated_data)
 
-        for item in orderitem_data:
-            OrderItem.objects.create(order=order, **item)
+            for item in orderitem_data:
+                OrderItem.objects.create(order=order, **item)
 
         return order
+
+    def update(self,instance,validated_data):
+        orderitem_data = validated_data.pop('items')
+        with transaction.atomic():
+            instance=super().update(instance,validated_data)
+
+            if orderitem_data is not None:
+                # Delete the exisiting items
+                instance.items.all().delete()
+                # Recreate items with updated data
+                for item in orderitem_data:
+                    OrderItem.objects.create(order=instance, **item)
+
+
+        return instance
+
+
 
 
     class Meta:
